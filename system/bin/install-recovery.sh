1eaf2befb422216f9fcc23968a2f0b47026a22b4
vishalk17 <vishalkapadi95@gmail.com>
2020-02-15 07:26:04 +0000

    chinese stock rom dump 5.8.028s

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,23 +1,6 @@
 #!/system/bin/sh
-  echo 1 > /sys/module/sec/parameters/recovery_done
-if ! applypatch -c EMMC:/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/recovery:13864236:b53262d8ed2f2d50f402a976728670b52792adde; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/boot:9844008:46f7a0cf1dae0cc1e117c1b956b12f33b1686b4c EMMC:/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/recovery b53262d8ed2f2d50f402a976728670b52792adde 13864236 46f7a0cf1dae0cc1e117c1b956b12f33b1686b4c:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
-  if applypatch -c EMMC:/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/recovery:13864236:b53262d8ed2f2d50f402a976728670b52792adde; then
-        echo 0 > /sys/module/sec/parameters/recovery_done
-        log -t recovery "Install new recovery image completed"
-        
-  if applysig /system/etc/recovery.sig recovery; then
-    sync
-    log -t recovery "Apply recovery image signature completed"
-  else
-    log -t recovery "Apply recovery image signature fail!!"
-  fi
-  
-  else
-        echo 2 > /sys/module/sec/parameters/recovery_done
-        log -t recovery "Install new recovery image not completed"
-  fi
+if ! applypatch -c EMMC:/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/recovery:13968684:a8be446d8ade10b72611a393e47708759ac4e05f; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/boot:9848104:03f2799c2d53f61809e6d0d82716231cea0d6563 EMMC:/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/recovery a8be446d8ade10b72611a393e47708759ac4e05f 13968684 03f2799c2d53f61809e6d0d82716231cea0d6563:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
-  echo 0 > /sys/module/sec/parameters/recovery_done
   log -t recovery "Recovery image already installed"
 fi
